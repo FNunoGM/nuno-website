c63d7333cb1355c0b6639019c5a697a2f09b5f2e
FNuno <fnuno91@gmail.com>
2026-02-16 22:13:36 +0000

    Update Hero and Navigation components for improved layout and styling

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -2,8 +2,8 @@
 
 export default function Hero() {
   return (
-    <section className="px-8 lg:px-16 pb-16 pt-8 lg:pt-12">
-      <div className="justify-items-end mt-100 text-xs md:text-lg tracking-[0.25em] text-japandi-dark/70">
+    <section className="min-h-screen px-8 lg:px-16 py-16 content-center">
+      <div className="justify-items-end text-xs md:text-lg tracking-[0.25em] text-japandi-dark/70">
         <p>
           Front-End Developer based in Portugal and working worldwide remotely.
         </p>
--- components/home/Navigation.tsx
+++ components/home/Navigation.tsx
@@ -4,22 +4,22 @@ import Link from "next/link";
 export default function Navigation() {
   return (
     <nav className="fixed bottom-0 left-0 right-0 px-8 lg:px-16 py-6 z-50">
-      <div className="flex gap-10">
+      <div className="flex gap-6 lg:gap-10 justify-center lg:justify-start">
         <Link
           href="/work"
-          className="font-serif text-s tracking-[0.35em] uppercase"
+          className="font-serif text-xs lg:text-sm tracking-[0.25em] lg:tracking-[0.35em] uppercase"
         >
           Work
         </Link>
         <Link
           href="/profile"
-          className="font-serif text-s tracking-[0.35em] uppercase"
+          className="font-serif text-xs lg:text-sm tracking-[0.25em] lg:tracking-[0.35em] uppercase"
         >
           Profile
         </Link>
         <Link
           href="/contact"
-          className="font-serif text-s tracking-[0.35em] uppercase"
+          className="font-serif text-xs lg:text-sm tracking-[0.25em] lg:tracking-[0.35em] uppercase"
         >
           Contact
         </Link>
