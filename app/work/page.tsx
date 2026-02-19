29e137ac4f6fd5addd8b5a260d3f4cd94cf7ca55
FNuno <fnuno91@gmail.com>
2026-02-19 17:12:42 +0000

    Update Work component: enhance project title, fix technology casing, and improve layout for better presentation

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -8,10 +8,10 @@ import Link from "next/link";
 export default function Work() {
   const projects = [
     {
-      title: "Luisa Hentsch",
+      title: "Portfolio Website for Luisa Hentsch",
       url: "https://luisahentsch.com/",
       image: "/work/luisa-hentsch.jpg",
-      technologies: ["NEXT.JS", "REACT", "TYPESCRIPT", "TAILWIND CSS"],
+      technologies: ["Next.js", "React", "TypeScript", "Tailwind CSS"],
     },
   ];
 
@@ -32,29 +32,43 @@ export default function Work() {
             <div className="w-full lg:w-1/2 space-y-12">
               {projects.map((project, index) => (
                 <div key={index} className="space-y-4">
-                  {/* Clickable Image */}
-                  <Link
-                    href={project.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <div className="overflow-hidden rounded-sm group w-3/4 justify-self-center">
-                      <Image
-                        src={project.image}
-                        alt={project.title}
-                        width={900}
-                        height={600}
-                        className="w-full object-cover transition-opacity duration-500 group-hover:opacity-70"
-                      />
+                  <div className="flex flex-col items-center">
+                    <div className="overflow-hidden rounded-sm group w-3/4">
+                      <Link
+                        href={project.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <Image
+                          src={project.image}
+                          alt={project.title}
+                          width={900}
+                          height={600}
+                          className="w-full object-cover transition-opacity duration-500 hover:opacity-70"
+                        />
+                      </Link>
                     </div>
-                  </Link>
+                  </div>
+
+                  {/* Title */}
+                  <div className="flex justify-center mt-6">
+                    <h2 className="font-sans text-xs tracking-[0.20em] uppercase">
+                      {project.title}
+                    </h2>
+                  </div>
+
+                  {/* <div className="flex justify-center mt-8">
+                    <h2 className="font-sans text-xs md:text-base tracking-[0.15em]">
+                      STACK
+                    </h2>
+                  </div> */}
 
                   {/* Technologies */}
-                  <ul className="flex gap-4 flex-wrap justify-center mt-5">
+                  <ul className="flex gap-4 flex-wrap justify-center mt-6">
                     {project.technologies.map((tech, i) => (
                       <li
                         key={i}
-                        className="font-sans text-xs md:text-base tracking-[0.15em] uppercase text-japandi-dark/60"
+                        className="font-sans text-xs tracking-[0.20em] uppercase"
                       >
                         {tech}
                       </li>
