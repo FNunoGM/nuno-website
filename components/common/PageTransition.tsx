0d20350db8b8c2548d1834b10d82cb67ed48409a
FNuno <fnuno91@gmail.com>
2026-02-23 10:59:49 +0000

    Add delay to transition in PageTransition component for smoother animations

--- components/common/PageTransition.tsx
+++ components/common/PageTransition.tsx
@@ -13,7 +13,7 @@ export default function PageTransition({
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 0.5, ease: "easeInOut" }}
+      transition={{ duration: 0.5, ease: "easeInOut", delay: 0.1 }}
     >
       {children}
     </motion.div>
